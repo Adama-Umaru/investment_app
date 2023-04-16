4a6df39e835d037c81495de009d223751da2ab14
Adama M. Umaru <127052260+Adama-Umaru@users.noreply.github.com>
2023-04-16 22:10:22 +0100

    Update investment.py

--- investment.py
+++ investment.py
@@ -3,12 +3,12 @@
 # App gives access to user for Transactions with realiability of it's services
 # >> Let's Write the Code <<
 
-#This is the Homepage of this App displaying grreetings to the User
+#This is the Homepage of this App displaying greetings to the User
 
 print(">>> YOU ARE WELCOME TO COHORT5 INVESTMENT APP <<<") 
 
                   # Here created values to different variables
-Trials = 3        # Total Attempts for Pin number input to access th eApp
+Trials = 3        # Total Attempts for Pin number input to access the App
 Userpin = 4321    # keypin to gain access to the app
 BalanceIwallet = 5000    # Account Opens with #5000 im balance
 rate = 0.5        # for every deposit you have 50% (Percentage) increment
@@ -17,7 +17,7 @@ vat = 0.07        # for every withdrawal there is a Charge of 7% of VAT Deductio
 choice = " "      # Assigned an empty string value to choose for operations of this App
 
 
-   # Here colects user assigned pin number
+   # Here collects user assigned pin number
 
 while Trials != 0:
     pin = int(input("Please Enter Your 4 digit pin Number: ")) 
@@ -47,14 +47,14 @@ while Trials != 0:
              else:
                 print('Sorry Yur Entered non Positive Number')
 
-  # Here Prompts User for Data colection for withdrawal Transaction           
+  # Here Prompts User for Data collection for withdrawal Transaction           
           elif(choice == "2"):                                                             
              Wallet_Name = input("please Enter Your Account Name: ")
              Amount_to_withdraw = float(input( "How Much Amount Are You Willing To Withdraw From Your Account:# "))
              vat = 0.07
              vat_per_calc = Amount_to_withdraw * vat 
 
-  # Here the VAt takes effect and be Deducted from the Wallet         
+  # Here the VAt takes effect and Deducted from the Wallet         
              if Amount_to_withdraw > 0:
                 if(BalanceIwallet - Amount_to_withdraw)>= 0:
                     BalanceIwallet = BalanceIwallet - vat_per_calc - Amount_to_withdraw    
